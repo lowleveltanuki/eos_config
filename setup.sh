2347c74d291fd81a7f86ebf6e0f0b03607efa6c6
tanuki <4wwAe749@proton.me>
2024-10-22 18:19:49 -0400

    Updated after reinstalling an OS.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-# Update mirrors
-# sudo mv new_mirrors /etc/pacman.d/mirrorlist
-# Additional repos
-sudo pacman -Sy flatpak
-# reboot
-
-# No availability on Flatpak (21SEP2024)
-yay -S veracrypt-git
-
-# Needs to interact
-sudo pacman -Sy code #OSS version
-code --install-extension ms-python.python
-
+### UNTESTED ###
+### Currently just a list of packages and commands ###
+sudo pacman -Sy flatpak pcsclite ccid opensc pcsc_tools veracrypt brave-bin code
+mkdir -p $HOME/.pki/nssdb
+certutil -d $HOME/.pki/nssdb -N
+sudo systemctl enable pcscd.service && sudo systemctl start pcscd.service
 # Flatpaks
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub com.brave.Browser
-flatpak install flathub org.keepassxc.KeePassXC
-flatpak install flathub com.protonvpn.www
-flatpak install flathub md.obsidian.Obsidian
-
-
+flatpak install flathub com.github.tchx84.Flatseal org.keepassxc.KeePassXC com.protonvpn.www md.obsidian.Obsidian
